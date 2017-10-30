6086ff21f7793fcf5843f7409dd00bb4caf78a4e
Roger Nordqvist <progdev.squirrel@gmail.com>
2017-10-30 23:07:19 +0100

    navbar finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule
